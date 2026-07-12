9293ecbe59fd6791758e5d09e588911547b73597
BwayCer <bwaycer@gmail.com>
2026-07-12 18:26:41 +0800

    feat(css_file): add 可以透過 build.yaml 動態更新允許輸出的路徑清單

--- packages/css_file/lib/builder.dart
+++ packages/css_file/lib/builder.dart
@@ -1,35 +1,135 @@
-import 'dart:async';
+import 'package:analyzer/dart/element/element.dart';
 import 'package:build/build.dart';
 
-Builder cssFileBuilder(BuilderOptions options) => CssFileBuilder();
+import 'package:glob/glob.dart';
+
+import './utils.dart';
+import './annotations.dart';
+
+Builder cssFileBuilder(BuilderOptions options) {
+  final outputPaths = _getOutputPaths(options, 'outputPaths');
+  return CssFileBuilder(outputPaths: outputPaths);
+}
+
+List<String>? _getOutputPaths(BuilderOptions options, String field) {
+  final rawPaths = options.config[field] as List?;
+
+  List<String>? parsedPaths;
+  if (rawPaths != null) {
+    // 將類型轉換為 `List<String>`
+    parsedPaths = rawPaths.map((e) => e as String).toList();
+  }
+
+  return parsedPaths;
+}
 
 class CssFileBuilder implements Builder {
-  // 定義輸入與輸出的映射關係: 輸入 ".dart"，輸出 ".g.dart"
+  late final List<String> _outputPaths;
+
+  CssFileBuilder({List<String>? outputPaths}) {
+    if (outputPaths == null) {
+      log.severe(
+        'The build_runner option "outputPaths" is required.'
+        ' Please configure it in your build.yaml.',
+      );
+      throw ArgumentError('Missing required config: "outputPaths"');
+    }
+    _outputPaths = outputPaths;
+  }
+
+  // NOTE:
+  // 目前 build.yaml 是 `r'$web$': ['.css']`, 此處是隨參數動態更新,
+  // 實際運行是正常的. 但後續可能要追蹤 issues/3295 看看怎麼變化.
+  // https://github.com/dart-lang/build/issues/3295
   @override
-  final Map<String, List<String>> buildExtensions = const {
-    '.dart': ['.g.dart'],
-  };
+  Map<String, List<String>> get buildExtensions => {r'$web$': _outputPaths};
 
   @override
   Future<void> build(BuildStep buildStep) async {
-    // 1. 讀取觸發此 BuildStep 的輸入檔案內容
-    final inputContent = await buildStep.readAsString(buildStep.inputId);
+    final assetGlob = Glob('lib/**.dart');
+    await for (final inputId in buildStep.findAssets(assetGlob)) {
+      final library = await buildStep.resolver.libraryFor(inputId);
 
-    // 2. 準備輸出的檔案路徑（把擴充檔名換成 .g.dart）
-    final outputId = buildStep.inputId.changeExtension('.g.dart');
+      final matchElementStream = _matchElementByAnnotationStream(
+        library,
+        _checkCssFileType,
+      );
+      await for (final _MatchedElement(:cssFilePath) in matchElementStream) {
+        if (!_outputPaths.contains(cssFilePath)) {
+          log.warning(
+            'The output path "$cssFilePath" is not allowed.'
+            ' Please add this path to the "outputPaths" option in your build.yaml.',
+          );
+          continue;
+        }
 
-    // 3. 組合出你要生成的固定代碼文字
-    final generatedCode =
-        '''
+        await sampleBuild(buildStep, inputId);
+      }
+    }
+  }
+
+  final _checkCssFileType = createTypeChecker(
+    CssFile,
+    inPackage: 'jaspr_css_file_builder',
+  );
+}
+
+class _MatchedElement {
+  final String cssFilePath;
+  final Element element;
+
+  const _MatchedElement(this.cssFilePath, this.element);
+}
+
+/// 以註解過濾元素
+Stream<_MatchedElement> _matchElementByAnnotationStream(
+  LibraryElement library,
+  CheckIsTargetType checkCssFileType,
+) async* {
+  final List<PropertyAccessorElement> allGetters = [];
+
+  // 收集該片段下的頂層 getters (例如外層的 List<StyleRule> get styles)
+  allGetters.addAll(library.getters);
+
+  // 收集該片段下所有類別內部的 getters (例如 class 裡的 static get styles)
+  for (final clazz in library.classes) {
+    allGetters.addAll(clazz.getters);
+  }
+
+  for (final element in allGetters) {
+    final annotations = element.metadata.annotations;
+
+    final hasCssFile = annotations.any(checkCssFileType);
+    if (!hasCssFile) continue;
+
+    final annotation = annotations.firstWhere(checkCssFileType);
+    // 取得 @CssFile 的 path 參數
+    final computedAnnotation = annotation.computeConstantValue();
+    final pathValue =
+        computedAnnotation?.getField('path')?.toStringValue() ?? '';
+    if (pathValue == '') continue;
+
+    yield _MatchedElement(pathValue, element);
+  }
+}
+
+Future<void> sampleBuild(BuildStep buildStep, AssetId inputId) async {
+  // 1. 讀取觸發此 BuildStep 的輸入檔案內容
+  final inputContent = await buildStep.readAsString(inputId);
+
+  // 2. 組合出你要生成的固定代碼文字
+  final generatedCode =
+      '''
 // **************************************************************************
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // **************************************************************************
 
-// 這是複製自 ${buildStep.inputId.path} 的內容：
+// 這是複製自 ${inputId.path} 的內容：
 $inputContent
 ''';
 
-    // 4. 將生成的代碼寫入檔案系統
-    await buildStep.writeAsString(outputId, generatedCode);
-  }
+  // 3. 準備輸出的檔案路徑
+  final outputId = AssetId(buildStep.inputId.package, 'web/${inputId.path}');
+  // 4. 將生成的代碼寫入檔案系統
+  await buildStep.writeAsString(outputId, generatedCode);
 }
