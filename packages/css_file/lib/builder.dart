5934ddf68e258024fb9d4b1a15be32678d2eb422
BwayCer <bwaycer@gmail.com>
2026-07-13 00:25:00 +0800

    feat(css_file): add Jaspr 官方的註解篩選邏輯

--- packages/css_file/lib/builder.dart
+++ packages/css_file/lib/builder.dart
@@ -1,7 +1,10 @@
 import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/type.dart';
 import 'package:build/build.dart';
+import 'package:source_gen/source_gen.dart';
 
 import 'package:glob/glob.dart';
+import 'package:jaspr/dom.dart' show StyleRule;
 
 import './utils.dart';
 import './annotations.dart';
@@ -86,31 +89,124 @@ Stream<_MatchedElement> _matchElementByAnnotationStream(
   LibraryElement library,
   CheckIsTargetType checkCssFileType,
 ) async* {
-  final List<PropertyAccessorElement> allGetters = [];
+  // 初始的候選元素: 所有的 `class` 和全域變數
+  // 參考 [GitHub: schultek/jaspr][fn01]
+  final elementList = [...library.topLevelVariables, ...library.classes]
+      .expand<Element>(
+        (e) => switch (e) {
+          final ClassElement e => [...e.fields, ...e.getters],
+          final TopLevelVariableElement e when e.isOriginDeclaration => [e],
+          TopLevelVariableElement(:final getter?) when e.isOriginGetterSetter =>
+            [getter],
+          _ => [],
+        },
+      );
 
-  // 收集該片段下的頂層 getters (例如外層的 List<StyleRule> get styles)
-  allGetters.addAll(library.getters);
+  final inputSystemPath = library.firstFragment.source.fullName;
 
-  // 收集該片段下所有類別內部的 getters (例如 class 裡的 static get styles)
-  for (final clazz in library.classes) {
-    allGetters.addAll(clazz.getters);
+  for (final element in elementList) {
+    final cssFile = _getCssFileAnnotation(
+      element,
+      checkCssFileType,
+      inputSystemPath,
+    );
+    if (cssFile == null) continue;
+
+    if (!_checkIsValidStyleRule(element, inputSystemPath)) continue;
+
+    yield _MatchedElement(cssFile.path, element);
+  }
+}
+
+/// 搜尋元素上是否有 `@CssFile` 註解並將其還原.
+///
+/// 當 `@CssFile` 沒有指定輸出路徑時會跳過該選項並以 `log.warning()` 輸出提示訊息.
+CssFile? _getCssFileAnnotation(
+  Element element,
+  CheckIsTargetType checkCssFileType,
+  String inputSystemPath,
+) {
+  final annotations = element.metadata.annotations;
+
+  final hasCssFile = annotations.any(checkCssFileType);
+  if (!hasCssFile) return null;
+
+  final annotation = annotations.firstWhere(checkCssFileType);
+  // 取得 @CssFile 的 path 參數
+  final computedAnnotation = annotation.computeConstantValue();
+  final pathValue = computedAnnotation?.getField('path')?.toStringValue() ?? '';
+  if (pathValue == '') {
+    String? elementTarget;
+    if (element.enclosingElement case final ClassElement clazz) {
+      elementTarget = ' ${clazz.name}.${element.name}';
+    } else {
+      elementTarget = ' ${element.name}';
+    }
+
+    log.warning(
+      '@CssFile has not file path.'
+      ' Failing element:$elementTarget in library $inputSystemPath.',
+    );
+    return null;
   }
 
-  for (final element in allGetters) {
-    final annotations = element.metadata.annotations;
+  return CssFile(pathValue);
+}
 
-    final hasCssFile = annotations.any(checkCssFileType);
-    if (!hasCssFile) continue;
+final TypeChecker _styleRuleChecker = TypeChecker.typeNamed(
+  StyleRule,
+  inPackage: 'jaspr',
+);
 
-    final annotation = annotations.firstWhere(checkCssFileType);
-    // 取得 @CssFile 的 path 參數
-    final computedAnnotation = annotation.computeConstantValue();
-    final pathValue =
-        computedAnnotation?.getField('path')?.toStringValue() ?? '';
-    if (pathValue == '') continue;
-
-    yield _MatchedElement(pathValue, element);
+/// 檢查對象是否為有效的 CSS 定義 (`List<StyleRule>`).
+/// 參考 [GitHub: schultek/jaspr][fn01].
+bool _checkIsValidStyleRule(Element element, String inputSystemPath) {
+  if (element.enclosingElement case final ClassElement clazz
+      when clazz.isPrivate || element.isPrivate) {
+    log.severe(
+      '@CssFile cannot be used on private classes or members.'
+      ' Failing element: ${clazz.name}.${element.name} in library $inputSystemPath.',
+    );
+    return false;
+  } else if (element.enclosingElement case final ClassElement clazz
+      when (element is FieldElement && !element.isStatic) ||
+          (element is GetterElement && !element.isStatic)) {
+    log.severe(
+      '@CssFile cannot be used on non-static class members.'
+      ' Failing element: ${clazz.name}.${element.name} in library $inputSystemPath.',
+    );
+    return false;
+  } else if (element.isPrivate) {
+    log.severe(
+      '@CssFile cannot be used on private variables or getters.'
+      ' Failing element: ${element.name} in library $inputSystemPath.',
+    );
+    return false;
   }
+
+  final type = switch (element) {
+    final PropertyAccessorElement e => e.type.returnType,
+    final PropertyInducingElement e => e.type,
+    _ => null,
+  };
+
+  if (type == null ||
+      !type.isDartCoreList ||
+      !_styleRuleChecker.isAssignableFromType(
+        (type as InterfaceType).typeArguments.first,
+      )) {
+    final prefix = switch (element.enclosingElement) {
+      ClassElement(:final name) => '$name.',
+      _ => '',
+    };
+    log.severe(
+      '@CssFile can only be applied on variables or getters of type List<StyleRule>.'
+      ' Failing element: $prefix${element.name} with type $type in library ${element.library?.firstFragment.source.fullName}.',
+    );
+    return false;
+  }
+
+  return true;
 }
 
 Future<void> sampleBuild(BuildStep buildStep, AssetId inputId) async {
@@ -133,3 +229,6 @@ $inputContent
   // 4. 將生成的代碼寫入檔案系統
   await buildStep.writeAsString(outputId, generatedCode);
 }
+
+// Footnote:
+// [fn01]: https://github.com/schultek/jaspr/blob/e4cc9d1/packages/jaspr_builder/lib/src/styles/styles_module_builder.dart#L50
